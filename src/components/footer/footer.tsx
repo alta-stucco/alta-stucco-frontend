1c0c03a9634b458cca7f937ddc510a55f6aea066
Parker Martin <parkerjmartin883@gmail.com>
2025-02-05 23:53:42 -0700

    adding github actions

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,12 +1,18 @@
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Footer = () => {
 
-    const date = new Date();
+    const date: Date = new Date();
     const year = date.getFullYear();
 
 
     return (
         <footer>
             <p>© {year} Alta Stucco</p>
+            <a href="/">
+                <FontAwesomeIcon icon={faFacebook} style={{ color: 'black' }}/>
+            </a>
         </footer>
     );
 }
