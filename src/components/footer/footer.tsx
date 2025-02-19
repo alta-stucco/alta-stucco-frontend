931ae49e2203f69778d1b0240d9014d0361fdaaf
Parker Martin <parkerjmartin883@gmail.com>
2025-02-18 17:49:10 -0700

    styling logos, temporary

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,4 +1,4 @@
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook, faLinkedin, faInstagram, faUpwork } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Navigation from "../navigation/navigation.tsx";
 import "./footer.css";
@@ -23,9 +23,14 @@ const Footer = () => {
             </a>
           </span>
           <p>© {year} Alta Stucco, all rights reserved</p>
-          <a href="/">
-            <FontAwesomeIcon icon={faFacebook} style={{ color: "black" }} />
-          </a>
+            <div className="social-icons">
+              <a href="https://facebook.com"><FontAwesomeIcon icon={faFacebook} style={{ color: "black" }} /></a>
+              <a href="https://linkedin.com"><FontAwesomeIcon icon={faLinkedin} style={{ color: "black" }} /></a>
+              <a href="https://instagram.com"><FontAwesomeIcon icon={faInstagram} style={{ color: "black" }} /></a>
+              <a href="https://instagram.com"><FontAwesomeIcon icon={faUpwork} style={{ color: "black" }} /></a>
+
+            </div>
+
         </div>
         <div className="footer-right">
           <Navigation />
