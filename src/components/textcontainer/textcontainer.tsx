35241b29309d5e310fed18884cca51a47281be3f
Parker Martin <parkerjmartin883@gmail.com>
2025-03-03 22:57:51 -0700

    basic home layout

---
+++ src/components/textcontainer/textcontainer.tsx
@@ -0,0 +1,17 @@
+export default function TextContainer() {
+    return (
+        <h1 className="text-center">
+            Full range of exterior services.
+            <br />
+
+            residential
+            commercial
+            custom design
+            partnerships
+            <br />
+
+            Schedule your
+            free consultation
+        </h1>
+    )
+}
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -6,19 +6,22 @@ import ImageGrid from "../../components/imageGrid/imageGrid.tsx";
 const Home = () => {
 
     return (
-        <div className="home-container">
-            <div>
-                <p>Is your stucco chipping or falling off? Do you have leaky gutters or windows? Is your stucco bulging? Is your chimney cracked and looking like a spider web? Does your stucco look shiny or glittering?
+       <div>
 
-                    Let us help restore your home or office exterior! <Link className="contact-home" to="/contact">Contact us</Link> for a free consultation!
-                </p>
-            </div>
-            <div className="portfolio-component">
-                <div className="portfolio-container">
-                <ImageGrid />
+            <div className="home-container">
+                <div>
+                    <p>Is your stucco chipping or falling off? Do you have leaky gutters or windows? Is your stucco bulging? Is your chimney cracked and looking like a spider web? Does your stucco look shiny or glittering?
+
+                        Let us help restore your home or office exterior! <Link className="contact-home" to="/contact">Contact us</Link> for a free consultation!
+                    </p>
+                </div>
+                <div className="portfolio-component">
+                    <div className="portfolio-container">
+                    <ImageGrid />
+                    </div>
                 </div>
             </div>
-        </div>
+       </div>
     );
 }
 
