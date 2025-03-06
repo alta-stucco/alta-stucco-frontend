4d9fd7e2054a8f7b2f2b3b58ddd97e7d2dd1e08e
Parker Martin <parkerjmartin883@gmail.com>
2025-03-06 16:31:24 -0700

    updating fonts

--- src/components/textcontainer/textcontainer.tsx
+++ src/components/textcontainer/textcontainer.tsx
@@ -1,27 +1,19 @@
-import './textcontainer.css'
+import "./textcontainer.css";
 
 export default function TextContainer() {
-    return (
-        <div className="text-center">
-            <p id="fullRange">
-                Full range of exterior services.
-            </p>
-            <p id="partnerships">
-                residential
-                <br/>
-                commercial
-                <br/>
-
-                custom design
-                <br/>
-
-                partnerships
-            </p>
-            <p id="scheduleConsultation">
-                Schedule your
-                free consultation
-            </p>
-
-        </div>
-    )
+  return (
+    <div className="text-center">
+      <p id="fullRange">Full range of exterior services.</p>
+      <p id="partnerships">
+        Residential
+        <br />
+        Commercial
+        <br />
+        Custom solutions
+        <br />
+        Partnerships
+      </p>
+      <p id="scheduleConsultation">Schedule your free consultation</p>
+    </div>
+  );
 }
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./home.css";
 
 import ImageGrid from "../../components/imageGrid/imageGrid.tsx";
@@ -6,59 +6,55 @@ import ImageGrid from "../../components/imageGrid/imageGrid.tsx";
 import CardHomeText from "../../components/card_text/card_text.tsx";
 
 const Home = () => {
+  const cardHomeData = [
+    {
+      title: "Stucco",
+      text: "Is your stucco cracking, chipped, bulging or shiny? Allow us to revitalize your stucco and ensure its longevity!",
+    },
+    {
+      title: "Stone, Brick & Rock",
+      text: "Add character or repair your existing stone to withstand Utah’s harsh weather conditions.",
+    },
+    {
+      title: "Fascia, Soffit & Gutters",
+      text: "Protect your roof and prevent moisture build up with expertly installed, high quality fascia and soffit.",
+    },
+    {
+      title: "Hardie Board",
+      text: "Durable, stylish and weather-resistant fiber cement board provides a beautiful finish that enhances your exterior.",
+    },
+    {
+      title: "Painting",
+      text: "Refresh your home or business with a new coat of paint. We utilize top-quality materials to ensure a stunning, long-lasting finish.",
+    },
+    {
+      title: "Partnerships",
+      text: "We work with a trusted network of specialists including; window companies, roofers, deck and patio designers, steel work experts and more.",
+    },
+  ];
+  return (
+    <>
+      <div className="home-container">
+        <div>
+          <p className="home-text">
+            Our mission is to repair and enhance the functionality, beauty and
+            value of your property!
+          </p>
+        </div>
+        <div className="portfolio-component">
+          <div className="portfolio-container">
+            <ImageGrid />
+          </div>
+        </div>
 
-
-    const cardHomeData = [
-        {
-            title: "Stucco",
-            text: "Is your stucco cracking, chipped, bulging or shiny? Allow us to revitalize your stucco and ensure its longevity!",
-        },
-        {
-            title: "Stone, Brick & Rock",
-            text: "Add character or repair your existing stone to withstand Utah’s harsh weather conditions.",
-        },
-        {
-            title: "Fascia, Soffit & Gutters",
-            text: "Protect your roof and prevent moisture build up with expertly installed, high quality fascia and soffit.",
-        },
-        {
-            title: "Hardie Board",
-            text: "Durable, stylish and weather-resistant fiber cement board provides a beautiful finish that enhances your exterior.",
-        },
-        {
-            title: "Painting",
-            text: "Refresh your home or business with a new coat of paint. We utilize top-quality materials to ensure a stunning, long-lasting finish."
-        },
-        {
-            title: "Partnerships",
-            text: "We work with a trusted network of specialists including; window companies, roofers, deck and patio designers, steel work experts and more.",
-        },
-
-    ]
-    return (
-       <>
-
-            <div className="home-container">
-                <div>
-                    <p>Is your stucco chipping or falling off? Do you have leaky gutters or windows? Is your stucco bulging? Is your chimney cracked and looking like a spider web? Does your stucco look shiny or glittering?
-
-                        Let us help restore your home or office exterior! <Link className="contact-home" to="/contact">Contact us</Link> for a free consultation!
-                    </p>
-                </div>
-                <div className="portfolio-component">
-                    <div className="portfolio-container">
-                    <ImageGrid />
-                    </div>
-                </div>
-
-                <div className="card-text-row" >
-                    {cardHomeData.map((card, index) => (
-                        <CardHomeText key={index} title={card.title} text={card.text} />
-                    ))}
-                </div>
-            </div>
-       </>
-    );
-}
+        <div className="card-text-row">
+          {cardHomeData.map((card, index) => (
+            <CardHomeText key={index} title={card.title} text={card.text} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;
